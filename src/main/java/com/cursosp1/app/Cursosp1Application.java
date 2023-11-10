b72ee36ab80daf5a6c6836ee1fbeaa441540af14
Micke Chi <ms270891@gmail.com>
2023-11-10 09:36:10 -0600

    se agrega cors

--- src/main/java/com/cursosp1/app/Cursosp1Application.java
+++ src/main/java/com/cursosp1/app/Cursosp1Application.java
@@ -2,6 +2,9 @@ package com.cursosp1.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Cursosp1Application {
@@ -9,5 +12,15 @@ public class Cursosp1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Cursosp1Application.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
 
 }
